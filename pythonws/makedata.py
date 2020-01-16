335107de2200ae5eca4bcedbc49e3f8ca1ba0d6c
yeomyeom <ysy9639@naver.com>
2020-01-15 16:54:33 -0800

    create ML data

--- pythonws/makedata.py
+++ pythonws/makedata.py
@@ -2,17 +2,28 @@ from pytube import YouTube
 from pydub import AudioSegment
 import subprocess
 
-#https://wikidocs.net/26366
+# https://wikidocs.net/26366
 
-name = "bad_ball_joint"                             #label name
-data_path = "C://Users//eom02//Documents//GitHub//Sound-mechanic//pythonws//ML_data//"           #ML data txt file 모아둔 곳
+# machine learning label setting
+name_list = ["bad_ball_joint", "bad_brake_pad", "engine_seizing_up", "failing_water_pump", "hole_in_muffler", "no_problem"]
+name = "no_problem"  # label name
 file_name = name + ".txt"
-wav_path = data_path + name + "_mp4//"   #save mp4 data and 그냥 잘린 데이터
-ml_path = data_path + name +"//"         #complete machine learning training data
 
-time_slice = 5
+# file path setting
+data_path = "D://pythonws//ML_data//"  # ML data txt 파일(<youtube video url>  1,10 이 형식으로 적어놓은) 모아둔 곳
+wav_path = data_path + name + "_mp4//"  # save mp4 data and 그냥 잘린 데이터
+ml_path = data_path + name + "//"  # complete machine learning training data
+
+# audio setting
+audio_sampling_rate = 44100  # sampling rate 44.1kHz
+audio_bit_rate = 128000  # bit rate 128k
+audio_channels = 1  # 1 is mono 2 is stereo, mono is default setting in android & IOS
+
+# make machine learning data long
+time_slice = 3
 time_interval = 1
 
+# start program!
 f = open(data_path + file_name, 'r')
 i = 0
 while True:
@@ -22,9 +33,9 @@ while True:
         print("download done" + str(i))
         break
     path = line.split('\t')[0]
-    times = line.split('\t')[-1]#time에 대한 list
-    time = times.split(',')#['0', '58']
-    time = list(map(int, time))
+    times = line.split('\t')[-1]  # time에 대한 list
+    time = times.split(',')  # ['0', '58']
+    time = list(map(int, time))  # [0, 58]
     try:
         sound = YouTube(path)
         print(sound.streams.filter(only_audio=True).all())
@@ -32,16 +43,19 @@ while True:
             sound_infos_str = str(sound_infos)
             sound_info = sound_infos_str.split(' ')
             sound_type = sound_info[2]
-            # webm을 wav 파일로 바꾸기 위해서는 mp4를 거쳐서 wav로 바꿈
             if sound_type.find('mp4') != -1:
-                #download video file
+                # download video file
                 sound_infos.download(output_path=wav_path, filename=str(i).zfill(2))
-                #convert mp4 to wav
-                #ffmpeg -i D://pythonws//ML_data//no_problem_mp4//00.mp4 -ab 128k -ac 2 -ar 48000 D://pythonws//ML_data//no_problem_mp4//00.wav
-                command = "ffmpeg -i " + wav_path + str(i).zfill(2) + ".mp4 -ab 128k -ac 2 -ar 48000 " + wav_path + str(i).zfill(2) + ".wav"
+                # convert mp4 to wav
+                # ffmpeg -i D://pythonws//ML_data//no_problem_mp4//00.mp4 -ab 128k -ac 2 -ar 44100 D://pythonws//ML_data//no_problem_mp4//00.wav
+                command = "ffmpeg -i " + wav_path + str(i).zfill(2) + ".mp4" + \
+                          " -ab " + str(audio_bit_rate) + \
+                          " -ac " + str(audio_channels) + \
+                          " -ar " + str(audio_sampling_rate) + \
+                          " " + wav_path + str(i).zfill(2) + ".wav"
                 print(command)
                 subprocess.call(command, shell=True)
-                #edit wav file
+                # edit wav file
                 wav = AudioSegment.from_wav(wav_path + str(i).zfill(2) + ".wav")
                 number_of_execute = 1
                 print(time)
@@ -49,7 +63,6 @@ while True:
                     print("print sec : " + str(sec))
                     if time.index(sec) % 2 == 0:
                         start = sec * 1000
-                        print(start)
                     else:
                         finish = sec * 1000
                         if number_of_execute == 1:
@@ -57,8 +70,9 @@ while True:
                             number_of_execute += 1
                         else:
                             cut_wav += wav[start:finish]
-                #store wav file
-                cut_wav.export(ml_path + "edit"+str(i).zfill(2)+".wav", format="wav", parameters=["-ab", "128k", "-ac", "2", "-ar", "48000"])
+                # store wav file
+                cut_wav.export(ml_path + "edit" + str(i).zfill(2) + ".wav", format="wav",
+                               parameters=["-ab", str(audio_bit_rate), "-ac", str(audio_channels), "-ar", str(audio_sampling_rate)])
                 print('download' + str(i).zfill(2) + "is finished")
                 i += 1
                 break
@@ -66,10 +80,11 @@ while True:
                 i += 1
                 print('fail')
 
-    except Exception as ex:
+    except Exception as error:
         i += 1
-        print(ex)
+        print("Error Occur : " + error)
         print('download fail')
+        pass
 
-#sound.streams.filter(only_audio=True).all()[1].download()
-#mime_type 확장자로 다운로드됨
+# sound.streams.filter(only_audio=True).all()[1].download()
+# mime_type 확장자로 다운로드됨
--- pythonws/show_data.py
+++ pythonws/show_data.py
@@ -3,30 +3,40 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 
-name = "bad_ball_joint"
-data_number = "00"
+name_list = ["bad_ball_joint", "bad_brake_pad", "engine_seizing_up", "failing_water_pump", "hole_in_muffler", "no_problem"]
+name = "engine_seizing_up"  # label name
 data_path = "D://pythonws//ML_data//"
 ml_path = data_path + name + "//"
-path = ml_path + data_number + ".wav"
-
 
 def wav_to_melspec():
-    print(path)
-    wav, sr = librosa.load(path)
-    wav_mel = librosa.feature.melspectrogram(y=wav, sr=sr)
-    '''
-    plt.figure(figsize=(12, 8))
-    librosa.display.specshow(wav_mel, y_axis='mel')
-    plt.title(path)
-    plt.show() #power_to_db를 안했을 때는 데이터에 frequency(y축)가 들쭉날쭉함
-    '''
-    plt.figure(figsize=(10, 4))
-    librosa.display.specshow(librosa.power_to_db(wav_mel, ref=np.max), y_axis='mel', sr=sr, x_axis='time')
-    plt.colorbar(format='%+2.0f dB')
-    plt.title('Mel-Spectrogram')
-    #plt.tight_layout()
-    plt.savefig(ml_path + data_number + ".png")
-    #plt.show()
+    i = 0
+    for exe_num in range(0, 100):
+        try:
+            data_number = str(i).zfill(2)
+            path = ml_path + "edit" + data_number + ".wav"
+            print(path)
+            wav, sr = librosa.load(path)
+            wav_mel = librosa.feature.melspectrogram(y=wav, sr=sr)
+            '''
+            plt.figure(figsize=(12, 8))
+            librosa.display.specshow(wav_mel, y_axis='mel')
+            plt.title(path)
+            plt.show() #power_to_db를 안했을 때는 데이터에 frequency(y축)가 들쭉날쭉함
+            '''
+            plt.figure(figsize=(10, 4))
+            librosa.display.specshow(librosa.power_to_db(wav_mel, ref=np.max), y_axis='mel', sr=sr, x_axis='time')
+            plt.colorbar(format='%+2.0f dB')
+            plt.title('Mel-Spectrogram')
+            #plt.tight_layout()
+            plt.savefig(ml_path + data_number + ".png")
+            #plt.show()
+            i += 1
+        except FileNotFoundError:
+            print("done")
+            break
+        except Exception as error:
+            print("Error : " + error)
+            break
 
 
 if __name__ == "__main__":
